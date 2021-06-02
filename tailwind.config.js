8af733fb3b63591e26a41edc8ae53b8889fce1ac
Dylan O'Leary <dylanolearydev@gmail.com>
2021-06-02 17:03:46 -0400

    Add basic tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,67 @@ module.exports = {
         "./src/pages/**/*.{js,ts,jsx,tsx}"
     ],
     darkMode: false,
-    theme: {},
+    theme: {
+        extend: {
+            colors: {
+                "gray-1": "#000000",
+                "gray-2": "#262626",
+                "gray-3": "#4d4d4d",
+                "gray-4": "#707070",
+                "gray-5": "#999999",
+                "gray-6": "#c4c4c4",
+                "gray-7": "#e0e0e0",
+                "gray-8": "#f2f2f2",
+                "gray-9": "#ffffff"
+            },
+            fontSize: {
+                "2xs": "6.5625rem",
+                xs: "0.75rem",
+                sm: "0.875rem",
+                base: "1rem",
+                lg: "10rem",
+                xl: "1.0625rem",
+                "2xl": "1.125rem",
+                "3xl": "1.1875rem",
+                "4xl": "1.3125rem",
+                "5xl": "1.5rem",
+                "6xl": "1.8125rem",
+                "7xl": "2.3125rem",
+                "8xl": "3.125rem",
+                "9xl": "4.4375rem"
+            },
+            fontFamily: {
+                "sf-pro-text": "SF Pro Text",
+                "sf-pro-display": "SF Pro Display"
+            },
+            boxShadow: {
+                "shadow-level-1":
+                    "0px 2px 4px 0px rgba(0,0,0,0.05), 0px 3px 9px 0px rgba(0,0,0,0.08)",
+                "shadow-level-2":
+                    "0px 4px 7px 0px rgba(0,0,0,0.1), 0px 8px 24px 0px rgba(0,0,0,0.15)",
+                "shadow-level-3":
+                    "0px 16px 32px 0px rgba(0,0,0,0.08), 0px 4px 8px 0px rgba(0,0,0,0.12)",
+                "field-default": "inset 0px 1px 2px 0px rgba(0,0,0,0.15)",
+                "field-error":
+                    "inset 0px 1px 2px 0px rgba(0,0,0,0.15), inset 0px -2px 0px 0px rgba(217,53,53,1)",
+                "button-innershadow":
+                    "inset -1px -1px 0px 0px rgba(0,0,0,0.1), inset 1px -1px 0px 0px rgba(0,0,0,0.1)"
+            },
+            borderRadius: {
+                none: "0",
+                xs: "0.0625rem",
+                sm: "0.125rem",
+                default: "0.13641467690467834rem",
+                lg: "0.25rem",
+                xl: "0.2874999940395355rem",
+                "2xl": "0.3125rem",
+                "3xl": "0.5rem",
+                "4xl": "0.8184880614280701rem",
+                "5xl": "1.5rem",
+                "6xl": "3.410367012023926rem",
+                full: "9999px"
+            }
+        }
+    },
     plugins: []
 };
